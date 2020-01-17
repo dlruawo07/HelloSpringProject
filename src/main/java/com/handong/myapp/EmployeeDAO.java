e790a7fdfbb05bed14ade753417eb2f9d7fef74d
이겸재 <@DESKTOP-DMLMO08>
2020-01-17 10:57:19 +0900

    first commit

---
+++ src/main/java/com/handong/myapp/EmployeeDAO.java
@@ -0,0 +1,8 @@
+package com.handong.myapp;
+
+import java.util.List;
+
+public interface EmployeeDAO 
+{
+    public List<EmployeeVO> getAllEmployees();
+}
---
+++ src/main/java/com/handong/myapp/EmployeeManager.java
@@ -0,0 +1,8 @@
+package com.handong.myapp;
+
+import java.util.List;
+
+public interface EmployeeManager 
+{
+    public List<EmployeeVO> getAllEmployees();
+}
